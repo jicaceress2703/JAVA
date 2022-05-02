5c1f6efe509def33d2bde73cea5fa760439fec63
jicaceress2703 <newphysicslife@gmail.com>
2022-05-02 16:16:55 -0500

    primer ejercicio condicional

--- Ejercicios_condicionales/src/ejercicios_condicionales/Ejercicios_condicionales.java
+++ Ejercicios_condicionales/src/ejercicios_condicionales/Ejercicios_condicionales.java
@@ -1,20 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicios_condicionales;
 
-/**
- *
- * @author ivan
- */
-public class Ejercicios_condicionales {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Ejercicios_condicionales {
+    public static boolean vocales(int x){
+        boolean aux=false;
+        switch(x){
+            case 97:
+                aux=true;
+            break;
+            case 101:
+                aux=true;
+            break;
+            case 105:
+                aux=true;
+            break;
+            case 111:
+                aux=true;
+            break;
+            case 117:
+                aux=true;
+            break;
+            case 65:
+                aux=true;
+            break;
+            case 69:
+                aux=true;
+            break;
+            case 73:
+                aux=true;
+            break;
+            case 79:
+                aux=true;
+            break;
+            case 85:
+                aux=true;
+            break;
+        }
+        return aux;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner entrada=new Scanner (System.in);
+       System.out.print("Ingrese el código ASCII que desea evaluar: ");
+       int codigo=Integer.parseInt(entrada.nextLine());
+       boolean identificador=vocales(codigo);
+       if(identificador==true){
+           System.out.println("El código ASCII coincide con una vocal.");
+       }else{
+           System.out.println("El código ASCII no representa una vocal.");
+       }
     }
     
 }
