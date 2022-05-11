e0daff43d00bf74d99c892575690681e156f38d2
jicaceress2703 <newphysicslife@gmail.com>
2022-05-10 22:21:15 -0500

    Inicio con ejercicios sobre cadenas

--- Arreglos/src/arreglos/Arreglos.java
+++ Arreglos/src/arreglos/Arreglos.java
@@ -1,20 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arreglos;
-
-/**
- *
- * @author ivan
- */
+import java.util.Scanner;
 public class Arreglos {
-
-    /**
-     * @param args the command line arguments
-     */
+    public static void contarChar(char ch,String cadena){
+        int cont=0;
+        for(int i=0;i<cadena.length();i++){
+            if(cadena.charAt(i)==ch){
+                cont=cont+1;
+            }
+        }
+        System.out.println(cont);
+    }
+    public static void verificar(String cadena){
+        int cont=0;
+        for(int i=0;i<cadena.length();i++){
+            int aux=cadena.charAt(i);
+            if(aux>=65 && aux<=90){
+                cont=cont+1;
+            }else if(aux>=97 && aux<=122){
+                cont++;
+            }            
+        }
+        if(cont==cadena.length()){
+            System.out.println("En la cadena solo hay letras.");
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada=new Scanner (System.in);
+        /*System.out.print("Ingrese la cadena de caracteres que desea analizar: ");
+        String cad=entrada.nextLine();
+        System.out.print("Ingrese el caracter que desea buscar: ");
+        char caracter=entrada.nextLine().charAt(0);
+        contarChar(caracter,cad);*/
+        System.out.print("Ingrese la cadena de caracteres que desea analizar: ");
+        String cad=entrada.nextLine();
+        verificar(cad);
     }
     
 }
